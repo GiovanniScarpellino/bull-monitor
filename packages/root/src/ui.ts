5fcc8ae93baf6d418830c95593f04c2f3ae39667
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2022-03-21 15:37:25 +0100

    Change ip to localhost

--- packages/root/src/ui.ts
+++ packages/root/src/ui.ts
@@ -4,7 +4,7 @@ export class UI {
   private uiVersion = pkg.version;
   private pkgName = '@bull-monitor/ui';
   // private cdnRoot = `https://cdn.jsdelivr.net/npm/${this.pkgName}@${this.uiVersion}`;
-  private cdnRoot = `http://192.168.129.87:6702`;
+  private cdnRoot = `http://localhost:6702`;
   // private resourcesRoot = this.cdnRoot + '/build';
   private resourcesRoot = this.cdnRoot;
   private buildCdnUrl(resource: string) {
